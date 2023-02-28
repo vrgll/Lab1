eee4e0d1fe193881df1becdcad28452112479bb3
vrgll <126551333+vrgll@users.noreply.github.com>
2023-02-28 10:17:16 +0300

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,29 @@
+groupmates = [
+    {
+        "name": "Александр",
+        "surname": "Иванов",
+        "exams": ["Информатика", "ЭЭиС", "Web"],
+        "marks": [4, 3, 5]
+    },
+    {
+        "name": "Иван",
+        "surname": "Петров",
+        "exams": ["История", "АиГ", "КТП"],
+        "marks": [4, 4, 4]
+    },
+    {
+        "name": "Кирилл",
+        "surname": "Смирнов",
+        "exams": ["Философия", "ИС", "КТП"],
+        "marks": [2, 3, 5]
+    }
+]
+
+def print_students(students):
+    average_grade = int(input())
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(30), u"Оценки".ljust(20))
+    for student in students:
+        sr = sum(student["marks"])/3
+        if sr >= average_grade:
+            print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20))
+print_students(groupmates)
